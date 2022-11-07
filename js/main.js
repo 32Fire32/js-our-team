bc492207ef2e38efa01a599d67eaf5173eaf5315
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-07 14:22:51 +0100

    creazione progetto e inserimento dati. Crea oggetti e array

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'strict mode'
+
+
+// mi creo gli oggetti
+const officeWorker1 = {
+    fullName: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg'
+}
+
+const officeWorker2 = {
+    fullName: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg'
+}
+
+const officeWorker3 = {
+    fullName: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg'
+}
+
+const officeWorker4 = {
+    fullName: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg'
+}
+
+const officeWorker5 = {
+    fullName: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg'
+}
+
+const officeWorker6 = {
+    fullName: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg'
+}
+
+// mi creo l'array di oggetti
+const team = [officeWorker1, officeWorker2, officeWorker3, officeWorker4, officeWorker5, officeWorker6];
+
